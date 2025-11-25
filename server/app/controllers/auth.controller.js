dfe06af6ffd4731297c0d02dd985e2632fe56542
JiyoungCha <isasegun@gmail.com>
2025-11-25 17:07:29 +0900

    251125 login 완성

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -7,6 +7,7 @@
 // import { logger } from "../middlewares/loggers/winston.logger.js";
 import { SUCCESS } from "../../configs/responseCode.config.js";
 import authService from "../services/auth.service.js";
+import cookieUitl from "../utils/cookie/cookie.uitl.js";
 import { createBaseResponse } from "../utils/createBaseResponse.util.js";
 
 // ----------------
@@ -32,9 +33,12 @@ async function login(req, res, next) {
     const body = req.body; // 파라미터 획득
 
     // 로그인 서비스 호출
-    const result = await authService.login(body);
+    const { accessToken, refreshToken, user } = await authService.login(body);
   
-    return res.status(SUCCESS.status).send(createBaseResponse(SUCCESS, result));
+    // Cookie에 RefreshToken 설정
+    cookieUitl.setCookieRefreshToken(res, refreshToken);
+
+    return res.status(SUCCESS.status).send(createBaseResponse(SUCCESS, {accessToken, user}));
   } catch(error) {
     next(error);
   }
---
+++ server/app/utils/cookie/cookie.uitl.js
@@ -0,0 +1,54 @@
+/**
+ * @file app/utils/cookie/cookie.util.js
+ * @description
+ */
+
+import dayjs from 'dayjs';
+
+// -----------------
+// private
+// -----------------
+/**
+ * 
+ * @param {import("express").Response} re 
+ * @param {string} cookieName 
+ * @param {string} cookieValue 
+ * @param {number} ttl 
+ * @param {boolean} httpOnlFlg 
+ * @param {boolean} secureFlg 
+ */
+function setCookie(res, cookieName, cookieValue, ttl, httpOnlFlg = true, secureFlg = false){
+  res.cookie(
+    cookieName,
+    cookieValue,
+    {
+      expires: dayjs().add(ttl, 'millisecond').toDate(),
+      httpOnly: httpOnlFlg,
+      secure: secureFlg,
+      sameSite: 'none',
+    }
+  );
+}
+
+// -----------------
+// public
+// -----------------
+/**
+ * 쿠키에 리프래시 토큰 설정
+ * @param {import("express").Response} res 
+ * @param {string} refreshToken 
+ */
+function setCookieRefreshToken(res, refreshToken) {
+  setCookie(
+    res,
+    process.env.JWT_REFRESH_TOKEN_COOKIE_NAME,
+    refreshToken,
+    parseInt(process.env.JWT_REFRESH_TOKEN_COOKIE_EXPIRY),
+    true,
+    true
+  );
+}
+
+export default {
+  setCookieRefreshToken,
+}
