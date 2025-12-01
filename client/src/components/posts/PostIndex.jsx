d4bbc53dff7e6c2166e1751142ce335876058475
JiyoungCha <isasegun@gmail.com>
2025-12-01 18:17:24 +0900

    251201 오류수정완료

--- client/src/components/posts/PostIndex.jsx
+++ client/src/components/posts/PostIndex.jsx
@@ -17,7 +17,7 @@ export default function PostIndex() {
         <div className="post-index-card-box">
           {
             list && list.map(item => {
-              return <div className="post-index-card" style={{backgroundImage: `url{"${item.image}"}`}} key={item.id}></div>
+              return <div className="post-index-card" style={{backgroundImage: `url(${item.image})`}} key={item.id}></div>
             })
           }
         </div>
