0bb943bb1efa3141f44b96b3c9f5f9a5eaef3206
JiyoungCha <isasegun@gmail.com>
2025-12-02 10:12:04 +0900

    251202 header 버튼 클릭시 이동 및 posts 더보기

--- client/src/components/common/UserInfo.jsx
+++ client/src/components/common/UserInfo.jsx
@@ -1,6 +1,16 @@
 import './UserInfo.css';
+import { useNavigate } from 'react-router-dom'
 
 export default function UserInfo() {
+  const navigate = useNavigate();
+
+  function redirectPosts() {
+    navigate('/posts'); 
+  }
+  function redirectPostCreate() {
+    navigate('/posts/create');
+  }
+
   return (
     <>
       <div className="user-info-container bottom-line">
@@ -11,8 +21,8 @@ export default function UserInfo() {
             <p className='user-info-stat-etc'>posts 1911</p>
           </div>
           <div className="user-info-btn-items">
-            <div className="user-info-btn" style={{backgroundImage: `url("/icons/btn-post-index.png")`}}></div>
-            <div className="user-info-btn" style={{backgroundImage: `url("/icons/btn-add.png")`}}></div>
+            <div className="user-info-btn" onClick={redirectPosts} style={{backgroundImage: `url("/icons/btn-post-index.png")`}}></div>
+            <div className="user-info-btn" onClick={redirectPostCreate} style={{backgroundImage: `url("/icons/btn-add.png")`}}></div>
             <div className="user-info-btn" style={{backgroundImage: `url("/icons/btn-user-index.png")`}}></div>
           </div>
         </div>
